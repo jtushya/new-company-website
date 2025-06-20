2bfd66f1d0577250c531f679cbfa165732b7e803
jtushya <techtushya@gmail.com>
2025-06-21 01:54:58 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   webpack: (config, { isServer }) => {
     // Fix fs module in browser environment
     if (!isServer) {
